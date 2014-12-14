9e33d7d243f93af52e7874deb074843f7e077b3f
Ian Ker-Seymer <i.kerseymer@gmail.com>
2014-12-13 19:11:31 -0700

    Update install script to use new repo

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+set -e
+
 if [ ! -d "$HOME/.yadr" ]; then
-    echo "Installing YADR for the first time"
-    git clone https://github.com/skwp/dotfiles.git "$HOME/.yadr"
+    echo "Installing YADR-alt for the first time..."
+    git clone https://github.com/ianks/yadr-alt.git "$HOME/.yadr"
     cd "$HOME/.yadr"
     [ "$1" = "ask" ] && export ASK="true"
     rake install
