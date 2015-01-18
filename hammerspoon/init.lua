73ff8dad4879ce1268a193e9ca5a674437f9d461
Ian Ker-Seymer <i.kerseymer@gmail.com>
2015-01-18 12:35:38 -0700

    Clean up hammerspoon init

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -60,7 +60,7 @@ end
 
 local hotkeys = {}
 
-function create_hotkeys()
+function createHotkeys()
   for key, fun in pairs(definitions) do
     local mod = hyper
     if string.len(key) == 2 and string.sub(key,2,2) == "c" then
@@ -73,28 +73,28 @@ function create_hotkeys()
   end
 end
 
-function rebind_hotkeys()
+function rebindKeys()
   for i, hk in ipairs(hotkeys) do
     hk:disable()
   end
   hotkeys = {}
-  create_hotkeys()
+  createHotkeys()
   alert.show("Rebound hotkeys.")
 end
 
-function apply_place(win, place)
+function applyPlace(win, place)
   local scrs = screen:allScreens()
   local scr = scrs[place[1]]
   grid.set(win, place[2], scr)
 end
 
-function apply_layout(layout)
+function applyLayout(layout)
   return function()
     for app_name, place in pairs(layout) do
       local app = appfinder.appFromName(app_name)
       if app then
         for i, win in ipairs(app:allWindows()) do
-          apply_place(win, place)
+          applyPlace(win, place)
         end
       end
     end
@@ -102,8 +102,8 @@ function apply_layout(layout)
 end
 
 function init()
-  create_hotkeys()
-  keycodes.inputSourceChanged(rebind_hotkeys)
+  createHotkeys()
+  keycodes.inputSourceChanged(rebindKeys)
   alert.show("Hammerspoon, at your service.")
 end
 
@@ -119,26 +119,26 @@ grid.MARGINY = 0
 local gw = grid.GRIDWIDTH
 local gh = grid.GRIDHEIGHT
 
-local gomiddle = {x = 1, y = 1, w = 4, h = 6}
-local goleft = {x = 0, y = 0, w = gw/2, h = gh}
-local goright = {x = gw/2, y = 0, w = gw/2, h = gh}
-local gobig = {x = 0, y = 0, w = gw, h = gh}
+local goMiddle = { x = 1,    y = 1, w = 4,    h = 6}
+local goLeft =   { x = 0,    y = 0, w = gw/2, h = gh}
+local goRight =  { x = gw/2, y = 0, w = gw/2, h = gh}
+local goFull =    { x = 0,    y = 0, w = gw,   h = gh}
 
 definitions = {
   a = saveFocus,
   [";"] = focusSaved,
 
-  h = setGrid(goleft),
+  h = setGrid(goLeft),
   j = focusify(),
-  k = setGrid(gobig),
-  l = setGrid(goright),
+  k = setGrid(goFull),
+  l = setGrid(goRight),
 
-  g = apply_layout(layout2),
+  g = applyLayout(layout2),
 
   d = grid.pushWindowNextScreen,
   r = hs.reload,
   q = function() appfinder.appFromName("hs"):kill() end,
-  e = hints.windowHints
+  p = hints.windowHints
 }
 
 mapped_apps = {
