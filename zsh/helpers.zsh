687995124bcf007138e4fdfdd1cdc2d59c307f2c
Ian Ker-Seymer <i.kerseymer@gmail.com>
2015-01-24 21:49:29 -0700

    Use replace as a helper function

--- zsh/helpers.zsh
+++ zsh/helpers.zsh
@@ -1,4 +1,22 @@
+#
+# Helper functions for zsh
+#
+
 tarball() {
   tar cvzf "$1".tar.gz "$1" &> /dev/null
   tar tf "$1".tar.gz
+}
+
+replace() {
+  if (( ! $+commands[ag] )); then
+    echo "Error: ag executable not found"
+    return 1
+  fi
+
+  find_this=$1
+  shift
+  replace_with=$1
+  shift
+
+  ag -l $find_this $* | xargs sed -i '' "s/$find_this/$replace_with/g"
 }
