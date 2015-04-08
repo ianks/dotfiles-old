51a17760c69a2069805baedc8a00486be059dd82
Ian Ker-Seymer <i.kerseymer@gmail.com>
2015-04-08 23:27:53 +0000

    Add new features to i3

--- apps/i3/i3status_wrapper.sh
+++ apps/i3/i3status_wrapper.sh
@@ -4,5 +4,6 @@ i3status -c ~/.i3/i3status.conf | while :
 do
     read line
     brightness=$(cat /sys/class/backlight/intel_backlight/brightness)
-    echo " ${brightness} | $line" || exit 1
+    volume=$(pulseaudio-ctl full-status | cut -d ' ' -f1)
+    echo " ${brightness} |  ${volume} | $line" || exit 1
 done
